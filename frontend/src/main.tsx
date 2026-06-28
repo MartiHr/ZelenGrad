32c82fbf7750e40f2a095a925af1dd79908c0555
MartiHr <81310884+MartiHr@users.noreply.github.com>
2026-06-28 15:38:12 +0000

    feat(ui): add icons to home page action cards
    Added Font Awesome icons to all role-based action cards on the
    home page for better visual scannability and a more polished
    landing experience.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,17 +3,18 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faChartLine, faCircleInfo, faClipboardList, faDrawPolygon, faFlag, faGaugeHigh, faHome, faLeaf, faListCheck, faMap, faRightFromBracket, faRightToBracket, faSignInAlt, faSignOutAlt, faTree, faTriangleExclamation, faUser, faUserPlus, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine, faCircleInfo, faClipboardList, faDrawPolygon, faFlag, faGaugeHigh, faHome, faLeaf, faListCheck, faMap, faRightFromBracket, faRightToBracket, faSignInAlt, faSignOutAlt, faTree, faTriangleExclamation, faUser, faUserPlus, faUsers, faBinoculars, faSeedling, faWrench, faShield, faGear, faCompass, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import { AuthProvider } from "./auth/AuthContext";
 import { router } from "./router";
 import "./styles/global.css";
 
 library.add(
-  faChartLine, faCircleInfo, faClipboardList, faDrawPolygon, faFlag,
-  faGaugeHigh, faHome, faLeaf, faListCheck, faMap,
-  faRightFromBracket, faRightToBracket, faSignInAlt, faSignOutAlt,
-  faTree, faTriangleExclamation, faUser, faUserPlus, faUsers
+  faArrowRight, faBinoculars, faChartLine, faCircleInfo, faClipboardList,
+  faCompass, faDrawPolygon, faFlag, faGaugeHigh, faGear,
+  faHome, faLeaf, faListCheck, faMap, faRightFromBracket,
+  faRightToBracket, faSeedling, faShield, faSignInAlt, faSignOutAlt,
+  faTree, faTriangleExclamation, faUser, faUserPlus, faUsers, faWrench
 );
 
 createRoot(document.getElementById("root")!).render(
